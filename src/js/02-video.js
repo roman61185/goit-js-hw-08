3f3276456ffa43c6b55a8b8b5d320630c58b09bc
roman61185 <117517302+roman61185@users.noreply.github.com>
2023-04-19 11:49:58 +0300

    в
    исправил

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,16 +12,18 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
 
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            break;
-        default:
-            break;
-    }
-});
+//     .then(function (seconds) {
+
+// }).catch(function (error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             break;
+//         default:
+//             break;
+//     }
+// });
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,18 @@
-const throttle = require('lodash.throttle');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+const throttle = require('lodash.throttle');
 const nameEmailMessage = document.querySelector('.feedback-form');
 const nameMail = nameEmailMessage.querySelector('input[name="email"]');
 const nameMessage = nameEmailMessage.querySelector('textarea[name="message"]');
@@ -7,48 +20,38 @@ const nameMessage = nameEmailMessage.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 let info = {};
+addForm();
 
 nameEmailMessage.addEventListener('input', throttle(collectInput, 500));
-
 function collectInput(evt) {
     info[evt.target.name] = evt.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(info));
+    localStorage.setItem("feedback-form-state", JSON.stringify(info));
     console.log(info);
 }
-
-// const savedInfo = localStorage.getItem(LOCALSTORAGE_KEY);
-// const parsedInfo = JSON.parse(savedInfo);
-// nameMail.value = parsedInfo.email;
-// nameMessage.value = parsedInfo.message;
-
-addForm();
 function addForm() {
     const savedInfo = localStorage.getItem(LOCALSTORAGE_KEY);
-    if (savedInfo) {
-        const parsedInfo = JSON.parse(savedInfo);
-        nameMail.value = parsedInfo.email;
-        nameMessage.value = parsedInfo.message;
-    }
+    const parsedInfo = JSON.parse(savedInfo);
+    if (parsedInfo) {
+
+        nameMail.value = parsedInfo.email || "";
+        nameMessage.value = parsedInfo.message || "";
+        info.email = parsedInfo.email || "";
+        info.message = parsedInfo.message || "";
+    } return '';
 }
+
 
 
 nameEmailMessage.addEventListener('submit', handleSubmit);
 function handleSubmit(evt) {
     evt.preventDefault();
-    evt.currentTarget.reset(); //очищаем поля формы
-    localStorage.removeItem(LOCALSTORAGE_KEY); //очищаем хранилище
+
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+
+    evt.currentTarget.reset();
+    console.log(info);
+
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
